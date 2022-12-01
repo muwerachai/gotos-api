b9f165f06315ad837524ac7e7bbc02dad5f81819
werachai <werachai@MacBook-Air-khxng-werachai.local>
2022-12-01 15:43:35 +0700

    province and category data

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require("path");
 
-const authRouter = require('./routes/authRoute');
-const placeRouter = require('./routes/placeRoute');
 
 const notFoundMiddleware = require('./middlewares/notFound');
 const errorMiddleware = require('./middlewares/error');
 const authenticateMiddleware = require("./middlewares/authenticate");
 
+const authRoute = require('./routes/authRoute');
+const placeRoute = require('./routes/placeRoute');
+
+const { addProvince } = require("./service/provinceService");
+const { addCategory } = require("./service/categoryService");
+
+const app = express();
+
 // const { sequelize } = require('./models');
 // sequelize.sync({ force:true});
-const app = express();
-app.use(cors());
+// addProvince();
+// addCategory(
+//   path.join(__dirname, "/public/img/attraction.jpg"),
+//   path.join(__dirname, "/public/img/restaurant.jpg"),
+//   path.join(__dirname, "/public/img/streetFood.jpg"),
+//   path.join(__dirname, "/public/img/nightLife.jpg")
+// );
+// console.log(path.join(__dirname, "/public/img/attraction.jpg"));
+
+ app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -21,8 +36,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.use('/auth',authRouter);
-app.use("/place", placeRouter);
+app.use('/auth',authRoute);
+app.use("/place", placeRoute);
 
 
 app.use(notFoundMiddleware);
